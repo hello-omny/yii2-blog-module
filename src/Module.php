7339ecb48847b2af1fd65735a60bfa23d20a6a60
Ruslan Zhovnerovskiy <hello.omny@gmail.com>
2019-07-31 21:35:49 +0300

    init

---
+++ src/Module.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace omny\yii2\blog\module;
+
+/**
+ * Class Module
+ * @package omny\yii2\blog\module
+ */
+class Module extends \yii\base\Module
+{
+    /** @var string */
+    public $controllerNamespace = 'omny\yii2\blog\module\controllers';
+
+}
